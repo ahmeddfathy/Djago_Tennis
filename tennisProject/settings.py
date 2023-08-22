a21e8a69a2d6d0f34d599618a8b0bdbbd9e7194c
ahmeddfathy <134372372+ahmeddfathy@users.noreply.github.com>
2023-08-22 13:55:45 +0300

    Update settings.py

--- tennisProject/settings.py
+++ tennisProject/settings.py
@@ -43,13 +43,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+ 
 ]
 
 ROOT_URLCONF = 'tennisProject.urls'
@@ -134,3 +135,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 MEDIA_URL ='/media/'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
